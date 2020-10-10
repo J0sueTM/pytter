9a47df07545a8565dac7fc1de9f8959e41b7eeb3
Josué Teodoro Moreira <jteodomo@gmail.com>
2020-10-10 02:56:42 -0300

    Update load_credentials.py

--- backend/load_credentials.py
+++ backend/load_credentials.py
@@ -3,11 +3,11 @@ import os.path
 
 # macros
 api_credentials = {} 
-api_credentials["CONSUMER_KEY"] = "CzKMJIHewYU8brmh0neTflE5i"
-api_credentials["CONSUMER_SECRET"] = "10CGMXLfRtUd3Y2yIRlIXwT5Yg84YGod9OdKfJlzdTvKExacYD"
-api_credentials["ACCESS_TOKEN"] = "1137817457941516288-eVqNEdLZowVUhN7v0EaTJNDZ8ViYXZ"
-api_credentials["ACCESS_SECRET"] = "WgjL7wr1eTlQxwqP566MfWql8cBqHkgQcnF4SbI4fZS8J"
-api_credentials["BEARER_TOKEN"] = "AAAAAAAAAAAAAAAAAAAAAHrhIQEAAAAArxQxNOB78ABd0GgAQAo3ywi3vSs%3DmQL8GISP87Sf1FPgASIoMbbaHNKsgJKQmvTJRiroiJ3SPCqVrX"
+api_credentials["CONSUMER_KEY"] = ""
+api_credentials["CONSUMER_SECRET"] = ""
+api_credentials["ACCESS_TOKEN"] = ""
+api_credentials["ACCESS_SECRET"] = ""
+api_credentials["BEARER_TOKEN"] = ""
 
 def writeCredentials():
     if not os.path.exists('./credentials/twitter_credentials.json'):
